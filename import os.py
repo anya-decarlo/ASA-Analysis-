fac32140b8f551c6414b389bbcdd14a81f563de9
anya-decarlo <182010635+anya-decarlo@users.noreply.github.com>
2024-12-05 01:23:52 -0800

    add data cleaning

---
+++ import os.py
@@ -0,0 +1,13 @@
+import os 
+import numpy as nm
+import pandas as pd 
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import scipy.stats as stats
+from statsmodels.regression.mixed_linear_model import MixedLMimport
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+
+
+
--- preprocess_Data.R
+++ preprocess_Data.R
@@ -18,3 +18,46 @@ read_data <- function(path, sep = ",", ...) {
   }
 
 
+describe <- function(x) { 
+  if(!inherits(x, "data.table")) { 
+    stop("Error: Input must be a data.table")
+    }
+  numeric_cols <- names(x)[sapply(x, is.numeric)]
+  categorical_cols <- names(x)[sapply(x, function(col)
+    is.factor(col) || is.character(col))]
+  date_cols <- names(x)[sapply(x, function(col)
+    inherits(col, c("IDate", "POSIXct", "PISIClt")))]
+  
+  Numeric <- data.table(t(x[, sapply(.SD, function(col) {
+    c(Min = min(col, na.rm = TRUE), 
+      Max = max(col, na.rm = TRUE), 
+      Mean = mean(col, na.rm =TRUE), 
+      Median = median(col, na.rm = TRUE),
+      SD = sd(col, na.rm = TRUE), 
+      N_missing = sum(is.na(col)))
+     }), .SDcols = numeric_cols])) 
+  
+    Numeric[, Variable := numeric_cols]
+    setcolorder(Numeric, "Variable")
+    
+    Categorical <- data.table( 
+      Variable = categorical_cols, 
+      Mode = x[, sapply(.SD, function(col) get_mode(col)), .SDcols = categorical_cols],
+      N_missing = x[, sapply(.SD, function(col) sum(is.na(col))), .SDcols = categorical_cols])
+      
+      Date <- data.table(t(x[, sapply(.SD, function(col) { 
+        c(Min = as.character(min(col, na.rm = TRUE)), 
+        Max = as.character(max(col, na.rm = TRUE)), 
+        Mean = as.character(mean(col, na.rm = TRUE)), 
+        N_missing = sum(is.na(col)))
+      }), .SDcols = date_cols]))
+      Date[, Variable := date_cols]
+      setcolorder(Date, "Variable")
+      
+      results <- list(
+        Numeric = Numeric,
+        Categorical = Categorical,
+        Date = Date)
+        
+      return(results)
+  }
