352c946ac04a2a0fb0fa26e3d30fd7fb3521ffc6
anya-decarlo <182010635+anya-decarlo@users.noreply.github.com>
2024-12-04 22:19:56 -0800

    clean_data

--- data_analysis.r
+++ data_analysis.r
@@ -1,4 +1,10 @@
 source("~/ASA-Analysis-/preprocess_Data.R")
 dat <- read_data("/Users/anyadecarlo/ctg_studies.csv")
 
-# Unique Study Types 
+str(dat)
+(dat)
+
+#Get Unique Values for Study Status 
+print(unique(dat$Study_Status))
+
+
